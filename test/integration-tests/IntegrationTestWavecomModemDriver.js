8cd1524f45bafbee29d3ccce75dfb19bb1dba159
Irwansyah <irwansyah@gmail.com>
2016-08-10 13:16:55 +0700

    Add IntegrationTestSmsDeviceWavecom.ts

--- test/integration-tests/IntegrationTestWavecomModemDriver.js
+++ test/integration-tests/IntegrationTestWavecomModemDriver.js
@@ -3,6 +3,7 @@ const WavecomModemDriver_1 = require('../../lib/wavecom/WavecomModemDriver');
 describe('WavecomModemDriver', function () {
     describe('identify()', function () {
         it('return the correct result', function (done) {
+            this.timeout(4000);
             let driver = new WavecomModemDriver_1.WavecomModemDriver();
             driver.identify('/dev/ttyUSB0')
                 .subscribe(r => {
--- test/integration-tests/IntegrationTestWavecomModemDriver.ts
+++ test/integration-tests/IntegrationTestWavecomModemDriver.ts
@@ -4,6 +4,7 @@ import {WavecomModemDriver} from '../../lib/wavecom/WavecomModemDriver';
 describe('WavecomModemDriver', function(){
     describe('identify()', function(){
         it('return the correct result', function(done){
+            this.timeout(4000);
             let driver = new WavecomModemDriver();
 
             driver.identify('/dev/ttyUSB0')
